ae47322f6a7fe5aeeb5af254c08d7e78182272dc
Jonathan <jonathan962911@gmail.com>
2020-04-05 12:10:16 -0500

    primer commit, estrcutura de aplicacion

---
+++ src/app/App.java
@@ -0,0 +1,7 @@
+package app;
+// [Modificador] tipo identificador ['valor']
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+}
